9ff8e7a9b946567bc50ac30d1853659be9dda505
chaya Thilakumara <chaya.t@ustocktrade.com>
2018-03-31 13:39:26 +0530

    initial commit

---
+++ src/main/java/com/test/SampleTest.java
@@ -0,0 +1,53 @@
+package com.test;
+
+import org.testng.annotations.Test;
+import com.pages.SignIn;
+import com.pages.SignUp;
+
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+
+public class SampleTest {
+	
+	
+	WebDriver driver;
+	SignUp signup;
+	SignIn signin;
+		
+		
+	@BeforeMethod
+	public void beforeMethod() { 
+		File file = new File("lib" + File.separator + "chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+		driver = new ChromeDriver();
+		signup = new SignUp(driver);
+		signin = new SignIn(driver);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) throws InterruptedException {
+	  signup.SignUpToApllication("anuradhaweer@gmail.com", "1qaz!QAZ", "1qaz!QAZ");
+	  signin.SignInToApllication("anuradhaweer@gmail.com", "1qaz!QAZ");
+  }
+  
+  @AfterMethod
+  public void afterMethod() {
+	  driver.close();
+  }
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" }
+    };
+  }
+}
